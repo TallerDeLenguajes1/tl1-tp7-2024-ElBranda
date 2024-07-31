16dde79d2ab13c2a3ba0c7761d15844c71a9aced
ElBranda <martinisaiasbrandalisijimenez@gmail.com>
2024-07-31 04:35:45 -0300

    tp7

---
+++ Calculadora.cs
@@ -0,0 +1,22 @@
+namespace EspacioCalculadora {
+    class Calculadora {
+        private double dato;
+
+        public void Sumar(double termino) {
+            dato += termino;
+        }
+        public void Restar(double termino) {
+            dato -= termino;
+        }
+        public void Multiplicar(double termino) {
+            dato *= termino;
+        }
+        public void Dividir(double termino) {
+            dato /= termino;
+        }
+        public void Limpiar() {
+            dato = 0;
+        }
+        public double Resultado { get => dato; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EspacioCalculadora;
+
 Console.WriteLine("Hello, World!");
+
+Calculadora calculadora = new Calculadora();
+
+while (true) {
+    Console.WriteLine("1. +");
+    Console.WriteLine("2. -");
+    Console.WriteLine("3. *");
+    Console.WriteLine("4. /");
+    Console.WriteLine("5. CLR");
+    Console.WriteLine("6. RES");
+    Console.WriteLine("7. EXIT");
+
+    int com = Convert.ToInt32(Console.ReadLine());
+
+    if (com == 5) calculadora.Limpiar();
+    else if (com == 6) Console.WriteLine(calculadora.Resultado);
+    else if (com == 7) break;
+    else {
+        double n = Convert.ToDouble(Console.ReadLine());
+
+        switch (com) {
+            case 1: calculadora.Sumar(n); break;
+            case 2: calculadora.Restar(n); break;
+            case 3: calculadora.Multiplicar(n); break;
+            case 4: calculadora.Dividir(n); break;
+        }
+    }
+}
